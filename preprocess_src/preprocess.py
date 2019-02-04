0a160917ad25262e28118d2b3b03526fffe99cd0
tkallep <tkpaaltonen@gmail.com>
2019-02-04 12:00:07 +0200

    add preprocess.py

---
+++ preprocess_src/preprocess.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+
+# files
+TRAIN_X = "../train_data/X_train_kaggle.npy"
+TEST_X = "../test_data/X_test_kaggle.npy"
+TRAIN_Y = "../train_data/y_train_final_kaggle.csv"
+GROUPS_ = "../train_data/groups.csv"
+
+# some functions for getting data
+def get_train_X():
+    return np.load(TRAIN_X)
+
+def get_test_X():
+    return np.load(TEST_X)
+
+def get_train_Y_dataframe():
+    return pd.read_csv(TRAIN_Y, delimiter=",")
+
+def get_train_Y_numpy():
+    return pd.read_csv(TRAIN_Y, delimiter=",").values
+
+def get_train_Y_list():
+    return pd.read_csv(TRAIN_Y, delimiter=",").values.tolist()
+
+def get_groups_dataframe():
+    return pd.read_csv(GROUPS_, delimiter=",")
+
+def get_groups_numpy():
+    return pd.read_csv(GROUPS_, delimiter=",").values
+
+def get_groups_list():
+    return pd.read_csv(GROUPS_, delimiter=",").values.tolist()
+
+# functions for preprocessing
+# 4.a) Straightforward reshape usingnumpy.ravel()ornumpy.resize()tomake 1280-dimensional vector from each sample.
+def ravelled(numpy_data):
+    rav = []
+    samples = numpy_data.shape[0]
+    for sample in range(samples):
+        rav.append(np.ravel(numpy_data[sample,:,:]))
+    return np.array(rav)
+
+# b) Compute the average over the time axis usingnumpy.mean()with ap-propriateaxisargument. This should make each sample 10-dimensional
+def meaned(numpy_data):
+    return np.mean(numpy_data, axis=2)
+
+def stdied(numpy_data):
+    return np.std(numpy_data, axis=2)
+
+# c) Compute the average and standard deviation over the time axis and con-catenate these to 20-dimensional feature vectors.
+def mean_std(numpy_data):
+    return np.concatenate((meaned(numpy_data), stdied(numpy_data)), axis=1)
+
+# d)  Choose which sensors you wish to use (remove those not wanted)
+# sensors_to_remove of unique numbers 0..9 eg [1, 2, 3, 5, 8]
+def remove_sensors(numpy_data, sensors_to_remove):
+    return np.delete(numpy_data, sensors_to_remove, axis=1)
+
+# accelerations and velocities to absolute values
+# deletes orientation values and returns only the rest
+def accelerations_velocities_abs(numpy_data):
+    numpy_data = remove_sensors(numpy_data, [0,1,2,3])
+    return np.array(list(map(abs, numpy_data)))
+
