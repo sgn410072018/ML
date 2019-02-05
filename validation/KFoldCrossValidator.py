ec6ee9b7e7384709cd60cd38249e3d75f861c8ec
SamuliAP <samuli.anola-pukkila@hotmail.fi>
2019-02-05 12:31:14 +0200

    Include non-meaned validation results

--- validation/KFoldCrossValidator.py
+++ validation/KFoldCrossValidator.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import GroupShuffleSplit
 from sklearn.preprocessing import LabelEncoder
-from validation.classifiers import classifiers
+import validation.classifiers as classifiers
 from pathlib import Path
 import numpy as np
 import pandas as pd
@@ -11,9 +11,12 @@ from itertools import groupby
 # how many times the cross validation shuffles and folds the data
 N_SHUFFLES_AND_FOLDS = 25
 
+CLASSIFIERS = classifiers.classifiers
+
 # file paths
 GROUPS_FILE_PATH = "../train_data/groups.csv"
 VALIDATION_RESULTS_FILE_PATH = "validation_results.csv"
+VALIDATION_RESULTS_MEANS_FILE_PATH = "validation_results_means.csv"
 PREPROCESSED_CSV_FILE_PATH = "../preprocessed-csv"
 
 
@@ -22,41 +25,33 @@ class KFoldCrossValidator:
     def __init__(self, test_file_path):
         self._groups_file = GROUPS_FILE_PATH
         self._X_train_file = test_file_path
-        self._label_encoder = LabelEncoder()
+        self._X = self._parse_X()[:, 3:].astype(float)
+        self._groups, self._y = self._parse_groups_data()
 
     def get_accuracy_scores(self):
-        X = self._parse_X()
-        X = self._label_encode(X)
-        X = X.astype(float)
-        X = X[:, 3:]
-
-        groupsdata = self._parse_groups_data()
-        groupsdata = self._label_encode(groupsdata)
-        groupsdata = groupsdata.astype(int)
-
-        results = self.run_cross_validation(X, groupsdata)
+        results = self.run_cross_validation()
         sorted_results = sorted(results, key=lambda tup: tup[0])
         return sorted_results
 
-    def run_cross_validation(self, X, groupsdata, folds=N_SHUFFLES_AND_FOLDS):
-        groups = groupsdata[:, 1]
-        y = groupsdata[:, 2]
-        gss = GroupShuffleSplit(random_state=1, n_splits=folds, test_size=0.2, train_size=0.8)
-        results = []
+    def run_cross_validation(self, folds=N_SHUFFLES_AND_FOLDS):
 
-        for train, test in gss.split(X, y, groups=groups):
-            for classifier in classifiers:
-                results.append(self._calc_model_accuracy(X, classifier, test, train, y))
+        gss = GroupShuffleSplit(random_state=1, n_splits=folds, test_size=0.2)
+        results = []
+        for train, test in gss.split(self._X, self._y, groups=self._groups):
+            for classifier in CLASSIFIERS:
+                results.append(self._calc_model_accuracy(classifier, test, train))
         return results
 
-    @staticmethod
-    def _calc_model_accuracy(X, classifier, test, train, y):
+    def _calc_model_accuracy(self, classifier, test, train):
         model = classifier[1]
-        x_train = np.take(X, train, axis=0)
-        y_train = np.take(y, train)
+        x_train = np.take(self._X, train, axis=0)
+        y_train = np.take(self._y, train)
+        x_test = np.take(self._X, test, axis=0)
+        y_test = np.take(self._y, test)
+
         model.fit(x_train, y_train)
-        pred = model.predict(np.take(X, test, axis=0))
-        score = accuracy_score(np.take(y, test), pred, classifier)
+        pred = model.predict(x_test)
+        score = accuracy_score(y_test, pred)
         return score, classifier[0]
 
     def _parse_X(self):
@@ -74,22 +69,22 @@ class KFoldCrossValidator:
                 groupsdata.append(values)
 
         groupsdata = np.array(groupsdata[1:])
-        return groupsdata
+
+        label_encoder = LabelEncoder()
+        groupsdata[:, 2] = label_encoder.fit_transform(groupsdata[:, 2])
+        groupsdata = groupsdata.astype(int)
+        return groupsdata[:, 1], groupsdata[:, 2]
 
     def print_accuracy_scores(self):
         results = self.get_accuracy_scores()
         for result in results:
             print("{}\n{}\n\n".format(result[0], result[1]))
 
-    def _label_encode(self, data):
-        data[:, 2] = self._label_encoder.fit_transform(data[:, 2])
-        return data
-
 
 def main():
     p = Path(PREPROCESSED_CSV_FILE_PATH)
     csv_filenames = list(p.glob('*.csv'))
-    with open(VALIDATION_RESULTS_FILE_PATH, 'w') as file:
+    with open(VALIDATION_RESULTS_MEANS_FILE_PATH, 'w') as file:
         writer = csv.writer(file, delimiter=';')
         writer.writerow(['Accuracy', 'Classifier Definition', 'Source File'])
         total_scores = []
@@ -99,6 +94,11 @@ def main():
         grouped_by_mean_and_file = group_by_mean_and_file(total_scores)
 
         writer.writerows(grouped_by_mean_and_file)
+
+        with open(VALIDATION_RESULTS_FILE_PATH, 'w') as file2:
+            writer2 = csv.writer(file2, delimiter=';')
+            writer2.writerow(['Accuracy', 'Classifier Definition', 'Source File'])
+            writer2.writerows(total_scores)
 
 
 def calc_classifier_scores_for_file(csv_filename):
@@ -111,13 +111,18 @@ def calc_classifier_scores_for_file(csv_filename):
 
 def group_by_mean_and_file(total_scores):
     grouped_mean_by_file = []
+
     total_scores = sorted(total_scores, key=lambda tup: tup[2])
-
     for k, g in groupby(total_scores, lambda tup: tup[2]):
-        file_scores = sorted(list(g), key=lambda tup: tup[1])
 
+        file_scores = sorted(list(g), key=lambda tup: tup[1])
         for k2, g2 in groupby(file_scores, lambda tup: tup[1]):
-            grouped_mean_by_file.append((np.mean([cell[0] for cell in list(g2)]), k2, k))
+            scores = np.array([cell[0] for cell in list(g2)])
+            mean = np.mean(scores, axis=0)
+            sd = np.std(scores, axis=0)
+
+            final = [x for x in scores if (x > mean - 2 * sd) and (x < mean + 2 * sd)]
+            grouped_mean_by_file.append((np.mean(final), k2, k))
 
     return grouped_mean_by_file
 
--- validation/classifiers.py
+++ validation/classifiers.py
@@ -1,6 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.ensemble import RandomForestClassifier
@@ -12,7 +13,7 @@ from sklearn.naive_bayes import GaussianNB
 # Some classifiers utilize multiprocessing, this should be <= the amount of logical CPU cores in a system
 threads = 4
 
-classifiers_pakollinen= [
+classifiers_pakollinen = [
     ('LinearDiscriminantAnalysis()', LinearDiscriminantAnalysis()),
     ('SVC(kernel="linear")', SVC(kernel="linear")),
     ('SVC(kernel="rbf")', SVC(kernel="linear")),
@@ -21,38 +22,55 @@ classifiers_pakollinen= [
     ('RandomForestClassifier(n_jobs=4, n_estimators=100)', RandomForestClassifier(n_jobs=4, n_estimators=100))
 ]
 
-classifiers= [
-    ('KNeighborsClassifier(n_jobs=threads, n_neighbors=20)', KNeighborsClassifier(n_jobs=threads, n_neighbors=20)),
-    ('KNeighborsClassifier(n_jobs=threads, n_neighbors=50)', KNeighborsClassifier(n_jobs=threads, n_neighbors=50)),
-    ('KNeighborsClassifier(n_jobs=threads, n_neighbors=100)', KNeighborsClassifier(n_jobs=threads, n_neighbors=100)),
-    ('KNeighborsClassifier(n_jobs=threads, n_neighbors=100, weights="distance")', KNeighborsClassifier(n_jobs=threads, n_neighbors=100, weights="distance")),
+classifiers = [
     ('LinearDiscriminantAnalysis()', LinearDiscriminantAnalysis()),
-    ('LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")', LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")),
-    ('LinearDiscriminantAnalysis(solver="svd")', LinearDiscriminantAnalysis(solver="svd")),
-    ('LinearDiscriminantAnalysis(solver="lsqr")', LinearDiscriminantAnalysis(solver="lsqr")),
-    ('SVC(gamma="auto")', SVC(gamma="auto")),
-    ('SVC(gamma=0.1)', SVC(gamma=0.1)),
-    ('SVC(kernel="sigmoid", gamma="auto")', SVC(kernel="sigmoid", gamma="auto")),
-    ('SVC(degree=1, gamma="auto")', SVC(degree=1, gamma="auto")),
-    ('SVC(degree=2, gamma="auto")', SVC(degree=2, gamma="auto")),
-    ('LogisticRegression(multi_class="auto", solver="liblinear", intercept_scaling=5)', LogisticRegression(multi_class="auto", solver="liblinear", intercept_scaling=5)),
-    ('LogisticRegression(n_jobs=threads, multi_class="auto", solver="newton-cg", max_iter=1000)', LogisticRegression(n_jobs=threads, multi_class="auto", solver="newton-cg", max_iter=1000)),
-    ('LogisticRegression(n_jobs=threads, class_weight="balanced", penalty="l2", multi_class="auto", solver="lbfgs")', LogisticRegression(n_jobs=threads, class_weight="balanced", penalty="l2", max_iter=2000, multi_class="auto", solver="lbfgs")),
-    ('LogisticRegression(n_jobs=threads, penalty="l2", multi_class="auto", solver="lbfgs")', LogisticRegression(n_jobs=threads, penalty="l2", multi_class="auto", solver="lbfgs")),
-    ('RandomForestClassifier(n_jobs=threads, n_estimators=20)', RandomForestClassifier(n_jobs=threads, n_estimators=20)),
-    ('RandomForestClassifier(n_jobs=threads, n_estimators=50)', RandomForestClassifier(n_jobs=threads, n_estimators=50)),
-    ('RandomForestClassifier(n_jobs=threads, n_estimators=100)', RandomForestClassifier(n_jobs=threads, n_estimators=100)),
-    ('RandomForestClassifier(n_jobs=threads, n_estimators=100, max_depth=5)', RandomForestClassifier(n_jobs=threads, n_estimators=100, max_depth=5)),
-    ('AdaBoostClassifier(n_estimators=20)', AdaBoostClassifier(n_estimators=20)),
-    ('AdaBoostClassifier(n_estimators=50)', AdaBoostClassifier(n_estimators=50)),
-    ('AdaBoostClassifier(n_estimators=100)', AdaBoostClassifier(n_estimators=100)),
-    ('AdaBoostClassifier(algorithm="SAMME", n_estimators=20)', AdaBoostClassifier(algorithm="SAMME", n_estimators=20)),
-    ('AdaBoostClassifier(algorithm="SAMME", n_estimators=50)', AdaBoostClassifier(algorithm="SAMME", n_estimators=50)),
-    ('AdaBoostClassifier(algorithm="SAMME", n_estimators=80)', AdaBoostClassifier(algorithm="SAMME", n_estimators=80)),
-    ('AdaBoostClassifier(algorithm="SAMME", n_estimators=100)', AdaBoostClassifier(algorithm="SAMME", n_estimators=100)),
-    ('AdaBoostClassifier(algorithm="SAMME", n_estimators=120)', AdaBoostClassifier(algorithm="SAMME", n_estimators=120)),
-    ('ExtraTreesClassifier(n_jobs=threads, n_estimators=20)', ExtraTreesClassifier(n_jobs=threads, n_estimators=20)),
-    ('ExtraTreesClassifier(n_jobs=threads, n_estimators=50)', ExtraTreesClassifier(n_jobs=threads, n_estimators=50)),
-    ('ExtraTreesClassifier(n_jobs=threads, n_estimators=50, criterion="entropy")', ExtraTreesClassifier(n_jobs=threads, n_estimators=50, criterion="entropy")),
-    ('GaussianNB()', GaussianNB())
+    ('LinearSVC()', LinearSVC()),
+    #('LinearSVC(loss="hinge")', LinearSVC(loss="hinge")),
+    #('LinearSVC(dual=False)', LinearSVC(dual=False)),
+    #('LinearSVC(C=0.2, dual=False)', LinearSVC(C=0.2, dual=False)),
+    #('LinearSVC(C=0.5, dual=False)', LinearSVC(C=0.5, dual=False)),
+    #('LinearSVC(C=0.9, dual=False)', LinearSVC(C=0.9, dual=False)),
+    #('LinearSVC(C=1.5, dual=False)', LinearSVC(C=1.5, dual=False)),
+    #('LinearSVC(C=2, dual=False)', LinearSVC(C=2, dual=False)),
+    #('LinearSVC(multi_class="crammer_singer", dual=False)', LinearSVC(multi_class="crammer_singer", dual=False)),
+    #('LinearSVC(intercept_scaling=2, dual=False)', LinearSVC(intercept_scaling=2, dual=False)),
+    #('LinearSVC(intercept_scaling=4, dual=False)', LinearSVC(intercept_scaling=4, dual=False)),
+    #('SVC(kernel="linear")', SVC(kernel="linear")),
+    #('SVC(kernel="rbf")', SVC(kernel="linear")),
+    #('SVC(kernel="rbf")', SVC(kernel="linear")),
+    #('LogisticRegression(n_jobs=4, multi_class="auto", solver="newton-cg", max_iter=1000)', LogisticRegression(n_jobs=4, multi_class="auto", solver="newton-cg", max_iter=1000)),
+    #('RandomForestClassifier(n_jobs=4, n_estimators=100)', RandomForestClassifier(n_jobs=4, n_estimators=100)),
+    #('KNeighborsClassifier(n_jobs=threads, n_neighbors=20)', KNeighborsClassifier(n_jobs=threads, n_neighbors=20)),
+    #('KNeighborsClassifier(n_jobs=threads, n_neighbors=50)', KNeighborsClassifier(n_jobs=threads, n_neighbors=50)),
+    #('KNeighborsClassifier(n_jobs=threads, n_neighbors=100)', KNeighborsClassifier(n_jobs=threads, n_neighbors=100)),
+    #('KNeighborsClassifier(n_jobs=threads, n_neighbors=100, weights="distance")', KNeighborsClassifier(n_jobs=threads, n_neighbors=100, weights="distance")),
+    #('LinearDiscriminantAnalysis()', LinearDiscriminantAnalysis()),
+    #('LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")', LinearDiscriminantAnalysis(solver="eigen", shrinkage="auto")),
+    #('LinearDiscriminantAnalysis(solver="svd")', LinearDiscriminantAnalysis(solver="svd")),
+    #('LinearDiscriminantAnalysis(solver="lsqr")', LinearDiscriminantAnalysis(solver="lsqr")),
+    #('SVC(gamma="auto")', SVC(gamma="auto")),
+    #('SVC(gamma=0.1)', SVC(gamma=0.1)),
+    #('SVC(kernel="sigmoid", gamma="auto")', SVC(kernel="sigmoid", gamma="auto")),
+    #('SVC(degree=1, gamma="auto")', SVC(degree=1, gamma="auto")),
+    #('SVC(degree=2, gamma="auto")', SVC(degree=2, gamma="auto")),
+    #('LogisticRegression(multi_class="auto", solver="liblinear", intercept_scaling=5)', LogisticRegression(multi_class="auto", solver="liblinear", intercept_scaling=5)),
+    #('LogisticRegression(n_jobs=threads, multi_class="auto", solver="newton-cg", max_iter=1000)', LogisticRegression(n_jobs=threads, multi_class="auto", solver="newton-cg", max_iter=1000)),
+    #('LogisticRegression(n_jobs=threads, class_weight="balanced", penalty="l2", max_iter=1000, multi_class="auto", solver="lbfgs")', LogisticRegression(n_jobs=threads, class_weight="balanced", penalty="l2", max_iter=1000, multi_class="auto", solver="lbfgs")),
+    #('LogisticRegression(n_jobs=threads, penalty="l2", multi_class="auto", solver="lbfgs", max_iter=1000)', LogisticRegression(n_jobs=threads, penalty="l2", multi_class="auto", solver="lbfgs", max_iter=1000)),
+    #('RandomForestClassifier(n_jobs=threads, n_estimators=20)', RandomForestClassifier(n_jobs=threads, n_estimators=20)),
+    #('RandomForestClassifier(n_jobs=threads, n_estimators=50)', RandomForestClassifier(n_jobs=threads, n_estimators=50)),
+    #('RandomForestClassifier(n_jobs=threads, n_estimators=100)', RandomForestClassifier(n_jobs=threads, n_estimators=100)),
+    #('RandomForestClassifier(n_jobs=threads, n_estimators=100, max_depth=5)', RandomForestClassifier(n_jobs=threads, n_estimators=100, max_depth=5)),
+    #('AdaBoostClassifier(n_estimators=20)', AdaBoostClassifier(n_estimators=20)),
+    #('AdaBoostClassifier(n_estimators=50)', AdaBoostClassifier(n_estimators=50)),
+    #('AdaBoostClassifier(n_estimators=100)', AdaBoostClassifier(n_estimators=100)),
+    #('AdaBoostClassifier(algorithm="SAMME", n_estimators=20)', AdaBoostClassifier(algorithm="SAMME", n_estimators=20)),
+    #('AdaBoostClassifier(algorithm="SAMME", n_estimators=50)', AdaBoostClassifier(algorithm="SAMME", n_estimators=50)),
+    #('AdaBoostClassifier(algorithm="SAMME", n_estimators=80)', AdaBoostClassifier(algorithm="SAMME", n_estimators=80)),
+    #('AdaBoostClassifier(algorithm="SAMME", n_estimators=100)', AdaBoostClassifier(algorithm="SAMME", n_estimators=100)),
+    #('AdaBoostClassifier(algorithm="SAMME", n_estimators=120)', AdaBoostClassifier(algorithm="SAMME", n_estimators=120)),
+    #('ExtraTreesClassifier(n_jobs=threads, n_estimators=20)', ExtraTreesClassifier(n_jobs=threads, n_estimators=20)),
+    #('ExtraTreesClassifier(n_jobs=threads, n_estimators=50)', ExtraTreesClassifier(n_jobs=threads, n_estimators=50)),
+    #('ExtraTreesClassifier(n_jobs=threads, n_estimators=50, criterion="entropy")', ExtraTreesClassifier(n_jobs=threads, n_estimators=50, criterion="entropy")),
+    #('GaussianNB()', GaussianNB())
 ]
